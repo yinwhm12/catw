ad25c6e9ad9cb7d331642e43155849939443dcdf
yinzhenwei <yinzhenwei@kuaifazs.com>
2017-04-11 18:34:39 +0800

    文章处理

--- catw/models/marticle.go
+++ catw/models/marticle.go
@@ -15,6 +15,7 @@ type Article struct {
 
 	User *User `json:"user" orm:"rel(fk)"`
 	EndType *EndType `json:"end_type" orm:"rel(fk)"`
+	ValueArticle *ValueArticle `json:"value_article" orm:"rel(one)"`
 }
 
 func (a *Article)TableName() string {
@@ -41,7 +42,7 @@ func GetArticleById(id int) (a *Article,err error)  {
 	return nil, err
 }
 
-func GetArticleByUser(id int)(articles []*Article,err error)  {
+func GetArticlesByUser(id int)(articles []*Article,err error)  {
 	if _,err =orm.NewOrm().QueryTable("article").
 		Filter("User",id).RelatedSel().All(&articles); err != nil{
 		return nil,err
@@ -49,10 +50,48 @@ func GetArticleByUser(id int)(articles []*Article,err error)  {
 	return articles,nil
 }
 
-func GetArticleByEndType(id int)(articles []*Article,err error)  {
+func GetArticlesByEndType(id int)(articles []*Article,err error)  {
 	if _,err = orm.NewOrm().QueryTable("article").
 		Filter("EndType",id).RelatedSel().All(&articles); err != nil{
 		return nil,err
 	}
 	return articles, nil
 }
+
+//通过rootId 类型获得
+func GetAticlesByRoot1Id(root1_id int)(articles []Article, err error)  {
+	o := orm.NewOrm()
+	_, err = o.Raw("SELECT * FROM acticle a INER JOIN end_type e on " +
+		"a.end_type_id = e.end_type_id WHERE e.root1_type_id = (" +
+		"SELECT root_1_type_id From root_1_type Where root_1_type_id = ?",root1_id).QueryRows(&articles)
+	if err != nil{
+		return nil, err
+	}
+	return articles, nil
+}
+
+//通过root2Id类型获得
+func GetAticlesByRoot2Id(root2_id int)(articles []Article,err error)  {
+	o := orm.NewOrm()
+	_, err = o.Raw("SELECT * FROM acticle a INER JOIN end_type e on " +
+		"a.end_type_id = e.end_type_id WHERE e.root2_type_id = (" +
+		"SELECT root_2_type_id From root_2_type Where root_2_type_id = ?",root2_id).QueryRows(&articles)
+	if err != nil{
+		return  nil, err
+	}
+	return articles, nil
+}
+
+//通过 LevelTypeId
+func GetAticlesByLevelTypeId(id int)(articles []Article,err error)  {
+	o := orm.NewOrm()
+	_, err = o.Raw("SELECT * FROM acticle a INER JOIN end_type e on " +
+		"a.end_type_id = e.end_type_id WHERE e.level_type_id = (" +
+		"SELECT level_type_id From level_type Where level_type_id = ?",id).QueryRows(&articles)
+	if err != nil{
+		return  nil, err
+	}
+	return articles, nil
+}
+
+//
---
+++ catw/models/mvalue_article.go
@@ -0,0 +1,48 @@
+package models
+
+import "github.com/astaxie/beego/orm"
+
+type ValueArticle struct {
+	ValueArticleId int `json:"value_article_id,omitempty" orm:"pk;column(value_article_id);auto"`
+	ReadCount int `json:"read_count,omitempty" orm:"column(read_count)"`
+	UpVout int `json:"up_vout,omitempty" orm:"column(up_vout)"`
+	CollectedCount int `json:"collected_count,omitempty" orm:"column(collected_count)"`
+
+
+	Article *Article `json:"article" orm:"reverse(one)"`
+}
+
+func (v *ValueArticle)TableName() string  {
+	return "value_article"
+}
+
+func init()  {
+	orm.RegisterModel(new(ValueArticle))
+}
+
+//阅读次数增加
+func AddOneByReadById(id int)(err error)  {
+	o := orm.NewOrm()
+	valueArticle := ValueArticle{ValueArticleId:id}
+	if o.Read(&valueArticle) == nil{
+		valueArticle.ReadCount++
+		if _,err = o.Update(&valueArticle); err != nil{
+			return  err
+		}
+	}
+	return nil
+
+}
+
+//点赞次数 增加
+func AddOneByUpById(id int)(err error)  {
+	o := orm.NewOrm()
+	valueArticle := ValueArticle{ValueArticleId:id}
+	if o.Read(&valueArticle) ==nil{
+		valueArticle.UpVout++
+		if _, err = o.Update(&valueArticle); err != nil{
+			return err
+		}
+	}
+	return nil
+}
